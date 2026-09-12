402cf30d6a60fbb19060c07b7f9f0119152bc0b7
Nikhilkoppad <144939564+Nikhilkoppad@users.noreply.github.com>
2026-09-13 02:13:38 +0530

    Load interview arena polish stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import './styles.css';
 import './ui-polish.css';
 import './mobile-polish.css';
 import './save-polish.css';
+import './interview-polish.css';
 import AppFinal from './AppFinal';
 import {ErrorBoundary} from './components/ErrorBoundary';
 import {CosmeticHud} from './components/CosmeticHud';
